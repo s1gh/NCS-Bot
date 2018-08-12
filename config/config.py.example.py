27fe35bc0a7f7be9cac3c0b4091122361d5d1487
Tommy Ingdal <tommy.ingdal@gmail.com>
2018-08-12 21:32:06 +0200

    Updated example config

---
+++ config/config.py.example.py
@@ -0,0 +1,11 @@
+config = {
+    'server': 'irc.server.net',
+    'port': 6667,
+    'nickname': '',
+    'nickname_alt': '',
+    'mask': '',
+    'realname': '',
+    'channels': [],
+    'command_prefix': '!',
+    'oper_password': ''
+}
