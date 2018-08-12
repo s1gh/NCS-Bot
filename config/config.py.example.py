ba30cbeb482806ae9f9cb3ca74a10249c0c1b772
Tommy Ingdal <tommy.ingdal@gmail.com>
2018-08-12 22:05:16 +0200

    Added oper password and tls options

--- config/config.py.example.py
+++ config/config.py.example.py
@@ -3,9 +3,11 @@ config = {
     'port': 6667,
     'nickname': '',
     'nickname_alt': '',
+    'tls': False,
     'mask': '',
     'realname': '',
     'channels': [],
     'command_prefix': '!',
+    'oper_username': '',
     'oper_password': ''
 }
